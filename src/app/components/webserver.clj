e5ff9265b4c94fbff5aa23ed1d43b6b37ff3cee0
@s.roertgen <sroertgen@gmail.com>
2024-11-20 15:22:40 +0100

    Endpoint working and validating against amb schema

--- src/app/components/webserver.clj
+++ src/app/components/webserver.clj
@@ -1,27 +1,87 @@
 (ns app.components.webserver
   (:require [io.pedestal.http :as http]
             [com.stuartsierra.component :as component]
-            [io.pedestal.http.route :as route]))
+            [io.pedestal.http.route :as route]
+            [nostr.core :as nostr]
+            [nostr.edufeed :as edufeed]
+            [cheshire.core :as json]
+            [json-schema.core :as json-schema]
+            [clojure.core.async :refer [go chan >! <!]]))
 
-(defn respond-hello [request]
-  {:status 200 :body "Hello, world!"})
+(defn validate [resources]
+  (let [schema (json-schema/prepare-schema
+                (-> "resources/schema.json"
+                    slurp
+                    (cheshire.core/parse-string true)))]
+    (try
+      (let [validation-results
+            (map (fn [resource]
+                   (try
+                     (when (json-schema/validate schema resource)
+                       {:valid true :resource resource})
+                     (catch Exception e
+                       {:valid false :error e :resource resource})))
+                 resources)]
+        (do
+          ;; Log invalid resources
+          (doseq [{:keys [error resource]} (filter #(not (:valid %)) validation-results)]
+            (println "Invalid resource:" resource "Error:" error))
+          ;; Return only valid resources
+          (->> validation-results
+               (filter :valid)
+               (map :resource))))
+      (catch Exception e
+        (println "Unexpected error during validation" e)
+        []))))
 
-(defn resources-by-user [request]
-  {:status 200 :body "hello user"})
+
+(defn ok [body]
+  {:status 200 :body body})
+
+;; Async pedestal
+(defn get-resources [request]
+  (let [result-chan (chan)]
+    (go
+      (let [npub (get-in request [:query-params :npub])
+            pk (get-in request [:query-params :pk])
+            filter (if pk
+                     {:kinds [30142] :authors [pk]}
+                     {:kinds [30142]})
+            resources (nostr/fetch-events "ws://localhost:7778" filter)
+            transformed (map #(edufeed/convert-30142-to-nostr-amb % true true) resources)
+            validate (validate transformed)
+            resp (if validate
+                   transformed
+                   {:response "Some error occured during validation against AMB Scheme. Better give you nothing than wrong data"})]
+        (>! result-chan (json/generate-string resp))))
+    result-chan))
+
+(def resources-by-user
+  {:name ::resources-by-user
+   :enter (fn [context]
+            (go
+              (let [result (<! (get-resources (:request context)))
+                    ;; TODO check against amb schema
+                    ]
+                (assoc context :response (ok result)))))})
 
 (def routes
   (route/expand-routes
-   #{["/greet" :get respond-hello :route-name :greet]
-     ["/user" :get resources-by-user :route-name :user]}))
+   #{["/resources" :get resources-by-user :route-name :resources]}))
+
+(def service-map
+  {::http/routes routes
+   ::http/type :jetty
+   ::http/port 8890})
 
 (defn create-service []
-  (http/create-server
-   {::http/routes routes
-    ::http/type :jetty
-    ::http/port 8890}))
+  (http/create-server service-map))
 
 (defn start []
   (http/start (create-service)))
+
+(comment
+  (http/stop (create-service)))
 
 (defrecord WebServer []
   component/Lifecycle
@@ -37,3 +97,23 @@
 
 (defn new-web-server []
   (map->WebServer {}))
+
+;; For interactive development
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                       (assoc service-map
+                              ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
+
+(comment
+  (start-dev)
+  (restart))
